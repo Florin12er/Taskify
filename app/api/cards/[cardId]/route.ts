d9dfd96f3ba02d1691eb8a47e395b502d29c98f1
Florin.12er <sebastianflorin603@gmail.com>
2025-02-27 00:25:11 +0100

    switch to mongodb

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,19 +1,22 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(
-    { params }: { params: { cardId: string } },
-) {
+export async function GET(request: NextRequest) {
   try {
     const { userId, orgId } = auth();
 
     if (!userId || !orgId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const { pathname } = new URL(request.url);
+    const cardId = pathname.split("/")[4];
+
     const card = await db.card.findUnique({
       where: {
-        id: params.cardId,
+        id: cardId,
         List: {
           Board: {
             orgId,
@@ -28,6 +31,11 @@ export async function GET(
         },
       },
     });
+
+    if (!card) {
+      return new NextResponse("Card not found", { status: 404 });
+    }
+
     return NextResponse.json(card);
   } catch (error) {
     return new NextResponse("Internal Error", { status: 500 });
